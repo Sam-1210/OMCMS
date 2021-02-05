d0641685404dd67d43f9f5b661efb257b7b0ba1f
Sam-1210 <dev.sam1210@gmail.com>
2021-02-05 20:46:14 +0530

    Fixed Cropped Top on Small Devices

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,12 +4,14 @@ function Home() {
     return (
     <div id="Home">  
         <div id="HomeBody" className="BlurBG">
-            <h1>Welcome to OMCMS</h1>
-            <h4>
-                Tired of Managing Meetings and Classes Links and Sharing them with Interested Audience?<br/> Well OMCMS is Here to Rescue.
-                Your One Stop Solution for Online Meetings and Classes Management,<br/>Register Today and Get Rid of Existing Rigid
-                Unorganised System.
-            </h4>
+            <div id="HomeContent">
+                <h1>Welcome to OMCMS</h1>
+                <h4>
+                    Tired of Managing Meetings and Classes Links and Sharing them with Interested Audience?<br/> Well OMCMS is Here to Rescue.
+                    Your One Stop Solution for Online Meetings and Classes Management,<br/>Register Today and Get Rid of Existing Rigid
+                    Unorganised System.
+                </h4>
+            </div>
         </div>
         <div id="HomeFooter" className="BlurBG">
         </div>
