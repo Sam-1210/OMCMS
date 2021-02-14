69f20a0f9944908ffa0bce78220274c3b181c20c
sam-1210 <dev.sam1210@gmail.com>
2021-02-14 16:43:54 +0530

    Theming #2.1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,21 @@ function About()
 {
     return (
         <div id="About">
-            <div className="ContentHeading">About</div>
+            <div className="ContentHeading">About OMCMS</div>
+            <div id="AboutBody">
+                <div id="AboutContent">
+                    OMCMS Is A Link Management System on An Abstracted View, But It Is Equipped with Combined Tools Like Staff and Schedule Management System, It Offers A Wide Variety Is Utilities to Enhance Your Online Workflow.
+                    <br/><br/>
+                    Centralized Information Systems Are Modern Day Need. During Covid-19 Pandemic We’ve Seen Non-Centralized System Becoming Rigid and Reducing Productivity. 
+                    <br/><br/>
+                    2020 Was The Year Of Online Meetings And Online Classes, Due To High Demand, We’ve Seen Online Meetings And Classes Systems Evolving, As A Result We Got Wide Variety  Of Meeting Apps, All Of Them Based On Simple Principle Create A Meeting And Share Links, But Sharing Of Those Links Is Tedious And Non-Centralized, Different Messaging And Email Platform Being Used For Sharing Those Links, Which Also Raises Some Privacy Concerns As Your Phone Number And Email Being Shared With Multiple Audience. 
+                    <br/><br/>
+                    With OMCMS All These Flaws Are Removed and A Simple Platform with Productivity Enhancing Features Is Introduced.
+                </div>
+                <div id="AboutFooter">
+                    The System Is Being Developed and Maintained By … Under Guidance Of … For ... 
+                </div>
+            </div>
         </div>);
 }
 
